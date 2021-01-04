9196286f88794b7c836abdc572e1e29991a40915
qpinot <quentin.pinot4@gmail.com>
2021-01-04 19:50:52 +0100

    Fin qui retourne au home + j'ai supp le msg dans la presentation

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ let d = new Date();
 let date = `${d.getDay()}-${d.getMonth()}-${d.getYear()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}: `;    
 
 
-
+/*
 const {
     PORT = 8080,
     PGHOST = "ec2-34-192-173-173.compute-1.amazonaws.com",
@@ -33,7 +33,19 @@ const {
     SESS_SECRET = "raboule_le_fric",
     SESS_LIFETIME = 24 * 60 * 60 * 1000 // 24h
 } = process.env;
+*/
 
+const {
+    PORT = 32222,
+    PGHOST = "localhost",
+    PGUSER = "postgres",
+    PGPASSWORD = "admin",
+    PGDATABASE = "Fidelity",
+    PGPORT = "5432",
+    SESS_NAME = "sid",
+    SESS_SECRET = "raboule_le_fric",
+    SESS_LIFETIME = 24 * 60 * 60 * 1000 // 24h
+} = process.env;
 
 const pool = new Pool(
     {
@@ -218,6 +230,7 @@ app.post('/questionnaire', (req, respo) =>
                                         console.log('Question numéro ' + i + ' ajoutée');
                                     }
                                 }
+                                respo.render('index.html');
                             }
                             catch (e) {
                                 console.log(date + "Error bdd -> " + e.stack);
